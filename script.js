215c36bf274261454e24fe5ef0fc5d03e1b1dd1c
Matthew Wilson <mwilson21@basecampcodingacademy.org>
2021-03-29 11:26:29 -0500

    adjusted speed

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ let userInput = 0;
 window.addEventListener('keydown', function (e) {
 
     document.querySelector('p').innerHTML = `You pressed ${e.key}`;
-    if (e.key == 'a') {
-        userInput -= 25;
+    if (e.key == 'a' && userInput > 0) {
+        userInput -= 15;
     }
-    else if (e.key == 'd') {
-        userInput += 25;
+    else if (e.key == 'd' && userInput < 1000) {
+        userInput += 15;
     }
-    console.log(document.querySelector('img').style.left)
+    console.log(window.innerWidth)
     document.querySelector('img').style.left = `${userInput}px`;
   }, false);
