b2b65c88e946d43b911188ec0b53c575d5856141
Matthew Wilson <mwilson21@basecampcodingacademy.org>
2021-03-29 12:04:59 -0500

    smoothed movement some

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 let userInput = 0;
+let x_position = 0;
 window.addEventListener('keydown', function (e) {
 
     document.querySelector('p').innerHTML = `You pressed ${e.key}`;
-    if (e.key == 'a' && userInput > 0) {
-        userInput -= 15;
+    if (e.key == 'a' && x_position > 0) {
+        userInput -= 3;
     }
-    else if (e.key == 'd' && userInput < 1000) {
-        userInput += 15;
+    else if (e.key == 'd' && x_position < 1000) {
+        userInput += 3;
     }
     console.log(window.innerWidth)
-    document.querySelector('img').style.left = `${userInput}px`;
+    document.querySelector('img').style.left = `${x_position}px`;
   }, false);
+function ManipulatePosition() {
+    if (userInput < 0) {
+        x_position -= 1
+        userInput += 1
+    }
+    else if (userInput > 0) {
+        x_position += 1
+        userInput -= 1
+    }
+}
+setInterval(ManipulatePosition, 10)
