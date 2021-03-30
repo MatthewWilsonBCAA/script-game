7399eaa072c4041874ee182db54eb66f6a1b5c8e
Matthew Wilson <mwilson21@basecampcodingacademy.org>
2021-03-30 16:19:32 -0500

    working on vertical movement

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
-let userInput = 0;
-let x_position = 0;
+let userXInput = 0;
+let userYInput = 0;
+let x_position = 1;
+let y_position = 1;
 window.addEventListener('keydown', function (e) {
 
     document.querySelector('p').innerHTML = `You pressed ${e.key}`;
     if (e.key == 'a' && x_position > 0) {
-        userInput -= 3;
+        userXInput -= 3;
     }
     else if (e.key == 'd' && x_position < 1000) {
-        userInput += 3;
+        userXInput += 3;
+    }
+    if (e.key == 'w' && y_position > 0) {
+        userXInput -= 3;
+    }
+    else if (e.key == 's' && y_position < 1000) {
+        userXInput += 3;
     }
     console.log(window.innerWidth)
     document.querySelector('img').style.left = `${x_position}px`;
+    document.querySelector('img').style.top = `${y_position}px`;
   }, false);
 function ManipulatePosition() {
-    if (userInput < 0) {
+    if (userXInput < 0) {
         x_position -= 1
-        userInput += 1
+        userXInput += 1
     }
-    else if (userInput > 0) {
+    else if (userXInput > 0) {
         x_position += 1
-        userInput -= 1
+        userXInput -= 1
+    }
+    if (userYInput < 0) {
+        y_position -= 1
+        userYInput += 1
+    }
+    else if (userYInput > 0) {
+        y_position += 1
+        userYInput -= 1
     }
 }
 setInterval(ManipulatePosition, 10)
